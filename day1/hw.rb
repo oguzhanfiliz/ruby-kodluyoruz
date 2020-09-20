80d9531740b29712bb661e7cde11a7bd40385c8e
Oğuzhan Filiz <oguzhan.filiz@ogr.dpu.edu.tr>
2020-09-20 03:27:30 +0300

    Day1-Kodluyoruz Rubby Bootcamp
    Hello world and starting kodluyoruz bootcamp

---
+++ day1/hw.rb
@@ -0,0 +1,9 @@
+puts 'Whats your name'
+    name = gets.chomp
+puts 'Whats your surname'
+    surname = gets.chomp
+
+fullname = name + '' + surname
+
+puts "Ad #{name} Soyad #{surname}"
+puts "#{fullname}"
